ffb6b496a88a4b37de7fecf9d8ed6970b2e15cd7
Elvis Brito <elvisbritodossantos61@gmail.com>
2020-12-30 01:29:00 -0300

    menu principal ajustado com scrolllistener

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Tmdb from './Tmdb'
 import './App.css';
 import MovieRow from './Components/MovieRow';
 import FeaturedMovie from './Components/FeaturedMovie';
+import Header from './Components/Header';
+
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState([]);
-  useEffect(()=>{
+  const [blackHeader, setBlackHeader] = useState(false)
+  useEffect(() => {
     const loadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
       // Pegando os Featured de Séries
-      let originals = list.filter(i=>i.slug === 'originals');
+      let originals = list.filter(i => i.slug === 'originals');
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
@@ -25,16 +28,32 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+        if (window.scrollY > 15){
+          setBlackHeader(true)
+        }else {
+          setBlackHeader(false);
+        }
+    }
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    }
+  }, [])
+
   return (
     <div className="page">
 
-    {featuredData && <FeaturedMovie item={featuredData} />}
+      <Header black={blackHeader} />
 
-     <section className="lists">
-       {movieList.map((item, key) => (
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
-     </section>
+      </section>
     </div>
   )
 }
